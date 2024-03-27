e2ef38b7b7758a3910961c581cafc7beef1a0a36
hanzhang.qin <hanzhang.qin@memblaze.com>
2024-03-27 19:01:11 +0800

    optimize align print

--- pba/pba.c
+++ pba/pba.c
@@ -2,7 +2,7 @@
  * @Author: Hanzhang Qin hanzhang.qin@memblaze.com
  * @Date: 2024-03-22 16:19:10
  * @LastEditors: Hanzhang Qin hanzhang.qin@memblaze.com
- * @LastEditTime: 2024-03-27 18:33:29
+ * @LastEditTime: 2024-03-27 18:58:14
  * @FilePath: \undefinedz:\useful\PBA\pba.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -78,6 +78,7 @@ void usage(char opt)
                 }
             }
             default:
+            {
                 if (!print_all)
                 {
                     print_all = 1;
@@ -88,6 +89,7 @@ void usage(char opt)
                 {
                     return;
                 }
+            }
         }
     }
 }
@@ -120,7 +122,7 @@ void print_pba(l2p_entry_t pba)
     u32 sblk    = blk * NAND_PLANE_NUM + pba.pl;
     U_FPA_44BIT pba44 = pba32_to_44(pba);
 
-    printf ("pba 0x%x: ch %d ln %d sb %d pg %d df %d blk %d pl %d xlun %d 44bit 0x%llx [lo32 hi32: 0x%x 0x%x]\n"  , pba.pba
+    printf ("pba32 0x%x: ch %d ln %d sb %d pg %d df %d blk %d pl %d xlun %d \t pba44 0x%llx [lo32 hi32: 0x%x 0x%x]\n"  , pba.pba
                                                                                                                     , ch
                                                                                                                     , lun
                                                                                                                     , sblk
@@ -202,7 +204,7 @@ l2p_entry_t pba44_to_pba32(U_FPA_44BIT pba44)
 void print_pba44(U_FPA_44BIT pba44)
 {
     l2p_entry_t pba = pba44_to_pba32(pba44);
-    printf ("pba44 0x%llx [lo32 hi32: 0x%x 0x%x]: ch %d ln %d sb %d pg %d df %d blk %d pl %d xlun %d 32bit 0x%x\n", pba44.val
+    printf ("pba44 0x%llx [lo32 hi32: 0x%x 0x%x]: ch %d ln %d sb %d pg %d df %d blk %d pl %d xlun %d \t pba32 0x%x\n", pba44.val
                                                                                                                 , pba44.lo32
                                                                                                                 , pba44.hi32
                                                                                                                 , pba44.ch
@@ -235,7 +237,7 @@ u32 print_pba44_by_string(const char *pba44_str[], u32 str_num)
 
         if (0 == string_to_u64(pba44_str[0], &lo32) || lo32 > INVALID_U32)
         {
-            printf("ERR PBA44 lo32 %s is not valid\n", pba44_str[0]);
+            printf("ERR: PBA44 lo32 %s is not valid\n", pba44_str[0]);
             return RET_FAIL;
         }
         else if (0 == string_to_u64(pba44_str[1], &hi32) || hi32 > INVALID_U32)
